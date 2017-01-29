fad384814bf826a0ed908f0d4fbc4b006127d09b
SmellydogCoding <SmellydogCoding@gmail.com>
2017-01-29 15:31:28 -0500

    librarian home page

--- models/book.js
+++ models/book.js
@@ -1,20 +1,30 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Book = sequelize.define('Book', {
-    id: {type: DataTypes.INTEGER,primaryKey: true},
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      validate: {
+        // name: property
+        // or name: {msg: "custom error message"}
+      }
+    },
     title: DataTypes.STRING,
     author: DataTypes.STRING,
     genre: DataTypes.STRING,
     first_published: DataTypes.INTEGER
-  },
-  {
+    },
+    {
       timestamps: false
     },
-   {
+    {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
       }
+    },
+    instanceMethods: {
+      // add some instance methods here
     }
   });
   return Book;
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,20 @@ var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  models.Book.findAll().then(function(books){
-    res.render('index', {books: books, title: "Library Manager"});
-  });
+  // models.Book.findAll().then(function(books) {
+  //   res.render('index', {books: books, title: "Library Manager"});
+  // });
+  res.render('index');
 });
 
 module.exports = router;
+
+// use the build method
+// use the create method to add rows to the database then redirect
+// use findById to find something by the id field
+// findAll({options}) order: [['column to order by','desc'],['second column to order by','ASC']]
+// use the update method to update existing rows
+// use the destroy method to delete rows
+// model.findBy.then function(model) {return model.method}.then redirect.catch(function(error){log error})
+// model.findBy.then function(model) {if model do stuff else error code}
+// before .catch .catch(error) {if (error.name === "SequelizeValidationError") {rerender page with error message error.errors} else {throw error}}
